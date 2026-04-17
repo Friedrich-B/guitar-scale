2bf4c9691d4389b2ed16edefe68d8276fc5855ab
Friedrich-B <friedrich.burmeister2003@gmail.com>
2026-04-18 01:47:55 +0200

    add modal (WIP)

--- src/components/TuningSelector.tsx
+++ src/components/TuningSelector.tsx
@@ -1,5 +1,4 @@
-import type { ReactElement } from "react";
-// import { NOTES, safeCopy } from "../helper";
+import { useState, type CSSProperties, type ReactElement } from "react";
 import s from './TuningSelector.module.scss';
 import { safeCopy } from "../helper";
 
@@ -11,17 +10,34 @@ interface Props {
 
 
 export function TuningSelector(props: Props): ReactElement {
-    const openSelectorModal = () => void {
-        // TODO: the selector should be a custom modal
-        // modal needs index to change the correct value
-        // modal gets setter passed
+    const [selectedString, setSelectedString] = useState<number|null>(null);
+    const [selectedNote, setSelectedNote] = useState<string|null>(null);
+    
+    const modalStyle: CSSProperties = selectedNote
+        ? {display: 'flex'}
+        : {};
+
+    const openSelectorModal = (
+        note: string,
+        stringIndex: number,
+    ): void => {
+        setSelectedString(stringIndex);
+        setSelectedNote(note);
+        // the modal should have a custom selector
+        // all notes displayed as square buttons, selected highlighted
+        // click on new note selects it and closes modal
     };
 
     // TODO now it would be nice to have NOTES as some kind of type
-    const setTuning = (newNote: string, stringIndex: number): void => {
+    const setTuning = (
+        newNote: string,
+        stringIndex: number,
+    ): void => {
         const newTuning = safeCopy(props.tuning);
         newTuning[stringIndex] = newNote;
 
+        setSelectedNote(null);
+        setSelectedString(null);
         props.setTuning(newTuning);
     };
 
@@ -29,15 +45,23 @@ export function TuningSelector(props: Props): ReactElement {
         return <div
             className={s.DisplayedNote}
             key={index}
+            onClick={() => openSelectorModal(rootNote, index)}
         >
             {rootNote}
         </div>;
-    });
+    }).reverse();
 
     return <div className={s.SelectorWrapper}>
         <div className={s.Selectors}>
             {...tunigSelectors}
         </div>
         <div className={s.Nut} />
+        <div
+            className={s.SelectorModal}
+            style={modalStyle}
+        >
+            {/* render note buttons */}
+            note: {selectedNote} string: {selectedString}
+        </div>
     </div>;
 }
